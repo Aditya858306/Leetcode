1ec05f729a45e818dff2db8a6f399a4aae2d2d5d
Aditya858306 <80695228+Aditya858306@users.noreply.github.com>
2023-02-09 19:14:44 +0530

    Time: 16 ms (86.36%), Space: 23.5 MB (86.36%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
@@ -10,6 +10,21 @@
  * };
  */
 class Solution {
+    void helper(TreeNode* root , string& ans){
+        if (!root) return;   
+        ans += to_string(root->val);
+        if (root->left) { //left side check
+            ans += '(';
+            helper(root->left, ans);
+            ans += ')';
+        }
+        if (root->right) { //right side check
+            if (!root->left) ans += "()"; //left side not present, but right side present
+            ans += '(';
+            helper(root->right, ans);
+            ans += ')';
+        }
+    }
 //     void helper(TreeNode* root , string& s , int l , int a){
 //         if(root == NULL){
 //             if(a==0){
@@ -30,56 +45,53 @@ class Solution {
 //         return;
          
 //     }
-    void helper(TreeNode* root , string& s , int l){
-        if(root == NULL){
-            return;
-        }
-        else if(root->left == NULL and root->right == NULL){
-            s = s + "(" + to_string(root->val) + ")";
-            return;
-        }
-        else if(root->left != NULL and root->right == NULL){
-            if(l!=0){
-                s = s + "(" + to_string(root->val);
-            }
-            helper(root->left, s , l+1);
-            if(l != 0){
-                s += ")";
-            }
+    //approch 2
+//     void helper(TreeNode* root , string& s , int l){
+//         if(root == NULL){
+//             return;
+//         }
+//         else if(root->left == NULL and root->right == NULL){
+//             s = s + "(" + to_string(root->val) + ")";
+//             return;
+//         }
+//         else if(root->left != NULL and root->right == NULL){
+//             if(l!=0){
+//                 s = s + "(" + to_string(root->val);
+//             }
+//             helper(root->left, s , l+1);
+//             if(l != 0){
+//                 s += ")";
+//             }
             
-        }
-        else if(root->left == NULL and root->right != NULL){
-            if(l == 0){
-                s += "()";
-            }
-            if(l != 0){
-                s = s  + "(" + to_string(root->val) + "()";
-            }
-            helper(root->right , s, l+1);
-            if(l!=0){
-                s += ")";
-            }
-        }
-        else{
-            if(l!=0){
-                s = s + "(" + to_string(root->val);
-            }
+//         }
+//         else if(root->left == NULL and root->right != NULL){
+//             if(l == 0){
+//                 s += "()";
+//             }
+//             if(l != 0){
+//                 s = s  + "(" + to_string(root->val) + "()";
+//             }
+//             helper(root->right , s, l+1);
+//             if(l!=0){
+//                 s += ")";
+//             }
+//         }
+//         else{
+//             if(l!=0){
+//                 s = s + "(" + to_string(root->val);
+//             }
             
-            helper(root->left, s , l+1);
-            helper(root->right, s , 1+1);
-            if(l!=0){
-                s+=")";
-            }
-        }
-    }
+//             helper(root->left, s , l+1);
+//             helper(root->right, s , 1+1);
+//             if(l!=0){
+//                 s+=")";
+//             }
+//         }
+//}
 public:
     string tree2str(TreeNode* root) {
-        string s = "" + to_string(root->val);
-        if(root->left == NULL and root->right == NULL){
-            return s;
-        }
-        
-        helper(root , s , 0);
+        string s;
+        helper(root , s);
         return s;
         // string s1 = "";
         // string s2 = "";
