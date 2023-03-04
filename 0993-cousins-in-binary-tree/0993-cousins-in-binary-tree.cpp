5b4f51ccc55c4b27fd2585b591dd2063f8a779eb
Aditya858306 <80695228+Aditya858306@users.noreply.github.com>
2023-03-04 18:58:55 +0530

    Time: 5 ms (34.75%), Space: 11.3 MB (10.83%) - LeetHub

---
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        queue<TreeNode*>q;
+        q.push(root);
+        unordered_map<int,int>mp;
+        while(!q.empty()){
+            int s = q.size();
+            for(int i = 0; i < s; i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                
+                if(temp->left){
+                    mp[temp->left->val] = temp->val;
+                    q.push(temp->left);
+                }
+                if(temp->right){
+                    mp[temp->right->val] = temp->val;
+                    q.push(temp->right);
+                }
+            }
+            
+            if(mp.find(x) != mp.end() and mp.find(y) != mp.end() and mp[x] != mp[y]){
+                return true;
+            }
+            mp.clear();
+        }
+        return false;
+    }
+};
