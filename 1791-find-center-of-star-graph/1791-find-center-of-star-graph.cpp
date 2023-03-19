fc0bb70cb504f6a74b0737bee983b3c3bfe085cc
Aditya858306 <80695228+Aditya858306@users.noreply.github.com>
2023-03-19 23:47:07 +0530

    Time: 282 ms (31.53%), Space: 83.7 MB (22.15%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        map<int,vector<int>>mp;
-        for(int i = 0; i<edges.size(); i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
+//         map<int,vector<int>>mp;
+//         for(int i = 0; i<edges.size(); i++){
+//             int u = edges[i][0];
+//             int v = edges[i][1];
             
-            mp[u].push_back(v);
-            mp[v].push_back(u);
-        }
+//             mp[u].push_back(v);
+//             mp[v].push_back(u);
+//         }
         
-        for(auto j : mp){
-            if(j.second.size() == edges.size()){
-                return j.first;
-            }
+//         for(auto j : mp){
+//             if(j.second.size() == edges.size()){
+//                 return j.first;
+//             }
+//         }
+//         return -1;
+        map<int,int> mp;
+        for (int i=0; i<edges.size(); i++){
+            mp[edges[i][0]]++;
+            mp[edges[i][1]]++;
+        }
+        for (auto val: mp){
+            if (val.second== (edges.size()))return val.first;
         }
         return -1;
     }
