15ec486722f6c856b9a58050bdf5bffd9bcf4752
Aditya Srivastava <80695228+Aditya858306@users.noreply.github.com>
2023-07-11 13:54:10 +0530

    Time: 25 ms (82.49%), Space: 8.8 MB (99.68%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,28 +1,59 @@
 class Solution {
-    void helper(int k, int n, vector<vector<int>>& ans, vector<int>& a, int index){
-        if(a.size()>k){
+    
+    void helper( vector<vector<int>>& ans, vector<int>& a, int n, int k, int index)
+    {
+        if(a.size() > k){
             return;
         }
+        
         if(a.size() == k){
             ans.push_back(a);
             return;
         }
         
-        
         for(int i = index; i<=n ; i++){
             a.push_back(i);
-            helper(k , n, ans, a, i+1);
+            helper(ans,a,n,k,i+1);
             a.pop_back();
-            
         }
+        
+        
     }
+    
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>>ans;
         vector<int>a;
-        int m = n;
-        helper(k, n, ans, a , 1);
+        vector<vector<int>>ans;
         
+        helper(ans, a, n, k,1);
         return ans;
     }
 };
+
+
+//     void helper(int k, int n, vector<vector<int>>& ans, vector<int>& a, int index){
+//         if(a.size()>k){
+//             return;
+//         }
+//         if(a.size() == k){
+//             ans.push_back(a);
+//             return;
+//         }
+        
+        
+//         for(int i = index; i<=n ; i++){
+//             a.push_back(i);
+//             helper(k , n, ans, a, i+1);
+//             a.pop_back();
+            
+//         }
+//     }
+// public:
+//     vector<vector<int>> combine(int n, int k) {
+//         vector<vector<int>>ans;
+//         vector<int>a;
+//         int m = n;
+//         solve(k, n, ans, a , 1);
+        
+//         return ans;
+//     }
