38b2a99d706b1a77071d7bb5aaeda4ba6e57c457
Aditya Srivastava <80695228+Aditya858306@users.noreply.github.com>
2023-07-29 18:44:38 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (18.59%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> ans;
+        
+        vector<int>ans1;
+        ans1.push_back(1);
+        ans.push_back(ans1);
+        if(rowIndex == 0){
+            return ans[0];
+        }
+        for(int i = 1;i<=rowIndex; i++){
+            vector<int>ans1;
+            for(int j = 0; j<ans[i-1].size()+1; j++){
+                if(j == 0 || j == ans[i-1].size()){
+                    ans1.push_back(1);
+                }
+                else if(j != 0 and j != ans[i-1].size()){
+                    int sum = ans[i-1][j-1] + ans[i-1][j];
+                    ans1.push_back(sum);
+                }
+            }
+            ans.push_back(ans1);
+        }
+        return ans[rowIndex];
+    }
+};
