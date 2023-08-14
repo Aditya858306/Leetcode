407a3cb67770f835fee0297ce62725ff38a23b4d
Aditya Srivastava <80695228+Aditya858306@users.noreply.github.com>
2023-08-14 20:34:49 +0530

    Time: 4 ms (27.64%), Space: 8.3 MB (39.85%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,32 +1,47 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count1 = 0, count2 = 0, count3 = 0;
+//         int count1 = 0, count2 = 0, count3 = 0;
         
-        for(int i = 0; i <nums.size(); i++){
-            if(nums[i] == 0){
-                count1++;
-            }
-            else if(nums[i] == 1){
-                count2++;
-            }
-            else if(nums[i] == 2){
-                count3++;
-            }
-        }
+//         for(int i = 0; i <nums.size(); i++){
+//             if(nums[i] == 0){
+//                 count1++;
+//             }
+//             else if(nums[i] == 1){
+//                 count2++;
+//             }
+//             else if(nums[i] == 2){
+//                 count3++;
+//             }
+//         }
         
-        for(int i = 0; i<nums.size(); i++){
-            if(count1){
-                nums[i] = 0;
-                count1--;
+//         for(int i = 0; i<nums.size(); i++){
+//             if(count1){
+//                 nums[i] = 0;
+//                 count1--;
+//             }
+//             else if(count2){
+//                 nums[i] = 1;
+//                 count2--;
+//             }
+//             else if(count3){
+//                 nums[i] = 2;
+//                 count3--;
+//             }
+//         }
+        int low = 0, mid = 0, high = nums.size()-1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
             }
-            else if(count2){
-                nums[i] = 1;
-                count2--;
+            else if(nums[mid] == 1){
+                mid++;
             }
-            else if(count3){
-                nums[i] = 2;
-                count3--;
+            else if(nums[mid] == 2){
+                swap(nums[mid], nums[high]);
+                high--;
             }
         }
     }
