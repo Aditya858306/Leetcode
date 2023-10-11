c0c8f8a675bea918150f6f22b0640543672fe4a5
Aditya Srivastava <80695228+Aditya858306@users.noreply.github.com>
2023-10-11 09:57:34 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (78.91%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2894-divisible-and-non-divisible-sums-difference/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2894-divisible-and-non-divisible-sums-difference.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int sum1 = 0;
+        for(int i = 1 ;i <= n; i++){
+            if(i%m != 0){
+                sum1 += i;
+            }
+        }
+        int sum2 = 0;
+        for(int i = 1 ;i <= n; i++){
+            if(i%m == 0){
+                sum2 += i;
+            }
+        }
+        int d = sum1 - sum2;
+        return d;
+    }
+};
